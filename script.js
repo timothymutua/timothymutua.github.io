1a88c4598c5bee8b2035c49cc3c285b614b6ea68
Timothy Mutua <timothy1mutua@gmail.com>
2026-07-02 21:46:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+var player;
+
+function onYouTubeIframeAPIReady() {
+player = new YT.Player('videoPlayer', {
+events: {
+'onReady': onPlayerReady
+}
+});
+}
+
+function onPlayerReady(event) {
+var pauseButton = document.getElementById('pauseButton');
+pauseButton.addEventListener('click', function() {
+player.pauseVideo();
+});
+}
+
+function pauseVideo() {
+player.pauseVideo();
+}
+function onPlayerReady(event) {
+event.target.playVideo();
+setTimeout(setShuffleFunction, 1000);           
+}
+function setShuffleFunction(){
+player.setShuffle(true);
+}
+
+
 let player;
 
 function onYouTubeIframeAPIReady() {
